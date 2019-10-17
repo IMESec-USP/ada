3c3842023c22510eedf207b23c418f22389622aa
danigfavero <danifavero@usp.br>
2019-10-17 19:09:13 -0300

    fixes refactoring

--- constants.py
+++ constants.py
@@ -6,10 +6,10 @@ env.read_env()
 TELEGRAM_API_TOKEN = env('TELEGRAM_API_TOKEN')
 GITHUB_SECRET = env('GITHUB_SECRET')
 
-CONVERSATIONS = env.list('CONVERSATIONS', [519013710], subcast=int)
+CONVERSATIONS = env.list('CONVERSATIONS', [131845033, -1001342903759], subcast=int)
 
 HEALTHCHECK_SERVICES = {'LH website':'https://lh.imesec.ime.usp.br',
                         'LH API':'https://api.lh.imesec.ime.usp.br/hacks'}
 
 HEALTHCHECK_SLEEP_AMOUNT = 2 * 60
-HEALTHCHECK_ANOMALY_THRESHOLD = 5
+HEALTHCHECK_ANOMALY_THRESHOLD = 3
