f68fe5dbcebc28f65732e72246b63c8ee900691e
r0zbot <r0zbot@users.noreply.github.com>
2019-10-21 18:26:23 -0300

    code review fixes

--- constants.py
+++ constants.py
@@ -1,5 +1,10 @@
 from environs import Env
 
+CHAT_ID_LIGHTNING_HACKS = -1001342903759
+CHAT_ID_IMESEC_CORE     = -1001284501077
+CHAT_ID_RAZGRIZONE      = 131845033
+CHAT_ID_R0ZBOT          = 211525815
+
 env = Env()
 env.read_env()
 
@@ -7,11 +12,17 @@ TELEGRAM_API_TOKEN = env('TELEGRAM_API_TOKEN')
 GITHUB_SECRET = env('GITHUB_SECRET')
 
 # Dict of conversations IDs followed by regex to match agains repository names
-convs_default = {
-	131845033: ".*",
-	-1001342903759: ".*lightning-hacks.*",
-	-1001284501077: "^((?!lightning-hacks).)*$",
-	211525815: ".*"
+default_conversations = {
+    # CHAT_ID_RAZGRIZONE: ".*",
+    # CHAT_ID_LIGHTNING_HACKS: ".*lightning-hacks.*",
+    # CHAT_ID_IMESEC_CORE: "^((?!lightning-hacks).)*$",
+    CHAT_ID_R0ZBOT: ".*"
 }
 
-CONVERSATIONS = env.dict('CONVERSATIONS', convs_default)
+def __conversation_subcast(s: str):
+    try:
+        return int(s)
+    except:
+        return s
+
+CONVERSATIONS = env.dict('CONVERSATIONS', default_conversations, subcast=__conversation_subcast)
