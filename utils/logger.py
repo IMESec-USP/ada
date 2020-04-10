fa935f1c00754386d344bdd2a2c0028491acec6f
pedro823 <pedro.leyton.pereira@usp.br>
2020-04-11 00:53:07 +0100

    Create logger with context

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,22 @@
 from datetime import datetime
 from sys import stdout
+
+
 class Logger:
+    class LoggerWithContext:
+        def __init__(self, parent_logger, context):
+            self._context = context
+            self.parent_logger = parent_logger
+
+        def log(self, message: str):
+            self.parent_logger.fpointer.write(f'[{datetime.now()}] [{self._context}] {message}\n')
+            self.parent_logger.fpointer.flush()
 
     def __init__(self, log_file: str):
         self.fpointer = open(log_file, 'a') if log_file else stdout
+
+    def with_class_name(self, obj):
+        return self.LoggerWithContext(self, obj.__class__.__name__)
     
     def log(self, message: str):
         self.fpointer.write(f'[{datetime.now()}] {message}\n')
