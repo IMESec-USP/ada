68aa9b8db5873af9f35ebce65580c8590c719973
Pedro Pereira <pedro.leyton.pereira@usp.br>
2020-10-03 20:26:56 +0100

    Removes Lightning-hacks chat from Ada (#16)

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
 from environs import Env
 
-CHAT_ID_LIGHTNING_HACKS = -1001342903759
 CHAT_ID_IMESEC_CORE     = -1001284501077
 CHAT_ID_RAZGRIZONE      = 131845033
 CHAT_ID_R0ZBOT          = 211525815
@@ -18,8 +17,7 @@ GITHUB_SECRET = env('GITHUB_SECRET')
 # Dict of conversations IDs followed by regex to match agains repository names
 default_conversations = {
     CHAT_ID_RAZGRIZONE: '.*',
-    CHAT_ID_LIGHTNING_HACKS: '^((?!dockerhub).)*lightning-hacks.*$',
-    CHAT_ID_IMESEC_CORE: '^((?!lightning-hacks).)*$',
+    CHAT_ID_IMESEC_CORE: '.*',
     CHAT_ID_R0ZBOT: '.*'
 }
 
